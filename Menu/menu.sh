565a7dcf51a9d289671774c7e7e555e24f99ec44
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2017-12-13 19:49:03 +0800

    Create menu.sh

---
+++ Menu/menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Menu
+
+echo -e "* Menu      : Senarai menu yang boleh digunakan"
+echo -e "* NewUser   : Membuat Akaun SSH Baru"
+echo -e "* Trial     : Membuat Akaun Trial"
+echo -e "* Remove    : Membuang Akaun Pengguna SSH"
+echo -e "* Check     : Semak Pengguna Yang Sedang Login"
+echo -e "* User      : Semak Senarai Pengguna SSH"
+echo -e "* ResServ   : Restart Service Dropbear, Webmin,"
+echo -e "              Squid3, OpenVPN dan SSH"
+echo -e "* Reboot    : Reboot VPS Anda"
+echo -e "* Speedtest : Speedtest VPS Anda"
+echo -e "* Info      : Memaparkan segala info tentang VPS Anda"
+echo -e "* About     : Informasi tentang script auto install"
+echo -e "* Exit      : keluar dari Putty/Connectbot/"
+echo -e "              JuiceSSH"
+echo -e ""
