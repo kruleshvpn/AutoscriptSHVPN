b5584631f12ed9c504146c6bed3245e81831c1d3
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2017-12-13 20:05:29 +0800

    Create Trial.sh

---
+++ Menu/Trial.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Script Auto Creating The Trial SSH User
+#Expired in One Day Only
+#Script by Khairul SHVPN
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+hari="1"
+Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
+
+useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Tahniah, Account Trial berjaya didaftarkan. Share detail kepada user!!!"
+echo -e ""
+echo -e "==== Details SSH Account ===="
+echo -e "Host         : $IP" 
+echo -e "Port OpenSSH : 22,143"
+echo -e "Port Dropbear: 80,443"
+echo -e "Port Squid   : 8080,3128,8080"
+echo -e "Port OpenVPN : 1194"
+echo -e "File OpenVPN : http://$IP:81/client.ovpn"
+echo -e "Username     : $Login "
+echo -e "Password     : $Pass"
+echo -e "-----------------------------"
+echo -e "Sah Sehingga : $exp"
+echo -e "============================="
+echo -e "Script by Khairul SHVPN"
+echo -e ""
