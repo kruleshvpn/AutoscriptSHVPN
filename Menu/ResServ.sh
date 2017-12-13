0eef8a9ac3b98b3f1d45e45860e95fc9ee73ebab
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2017-12-13 20:24:04 +0800

    Create ResServ.sh

---
+++ Menu/ResServ.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script for Auto-Restarting service Dropbear, Webmin, Squid3, OpenVPN, OpenSSH
+# Script by Khairul SHVPN
+service dropbear restart
+service webmin restart
+service squid3 restart
+service openvpn restart
+service ssh restart
