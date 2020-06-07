6a5c4488345dc075076b35acdf894be7ea718788
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2020-06-08 01:07:34 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -370,15 +370,3 @@ cd
 echo "" | sudo tee -a /etc/ssh/sshd_config
 echo "Port 22" | sudo tee -a /etc/ssh/sshd_config
 rm -f /root/debian9.sh
-cd
-wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/kruleshvpn/AutoscriptSHVPN/master/conf/sources.list.debian10"
-cd
-apt-get update
-apt install php7.0 mysql-server mysql-client libapache2-mod-php7.0 phpmyadmin
-cd
-sudo ln -s /usr/share/phpmyadmin /home/vps/public_html
-echo "" | sudo tee -a /etc/apache2/apache2.conf
-echo "Include /etc/phpmyadmin/apache.conf" | sudo tee -a /etc/apache2/apache2.conf
-echo "" | sudo tee -a /etc/apache2/apache2.conf
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-sudo a2enconf phpmyadmin.conf
