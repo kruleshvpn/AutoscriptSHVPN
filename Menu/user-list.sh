a7cb819727cf3814f7bad117b7465dcb3e84d2c3
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2020-06-20 01:34:19 +0800

    Update user-list.sh

--- Menu/user-list.sh
+++ Menu/user-list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "-------------------------------"
 echo "USERNAME          EXPIRED DATE "
-echo "-------------------------------"
+echo "==============================="
 echo -e ""
 while read expired
 do
@@ -9,11 +9,14 @@ do
         ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
         exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
         if [[ $ID -ge 1000 ]]; then
-        printf "%-17s %2s\n" "$AKUN" "$exp"
+        echo "------------------------------------------"
+        printf "%-17s %2s\n" "Username : $AKUN , Account Expired On :" "$exp"
         fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo "-------------------------------"
+echo "------------------------------------------"
+echo ""
+echo "==============================="
 echo "Jumlah User Account: $JUMLAH User"
 echo "-------------------------------"
 echo -e ""
