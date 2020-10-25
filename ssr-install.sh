28c07f77c2eed136a8e98997ebba10e62cc4a126
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2020-10-25 21:57:58 +0800

    Update ssr-install.sh

--- ssr-install.sh
+++ ssr-install.sh
@@ -1,1 +1,8 @@
-s
+#!/bin/bash
+#
+# Script By Khairul SHVPN
+# SHVPN Your Internet Speed Expert
+# ==================================================
+
+snap install shadowsocks-libev;
+mkdir -p /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev;
