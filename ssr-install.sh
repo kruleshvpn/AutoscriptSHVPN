6cc90ce828c9972f01c5e1cf1859b32c89b469eb
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2020-10-25 22:08:00 +0800

    Update ssr-install.sh

--- ssr-install.sh
+++ ssr-install.sh
@@ -7,3 +7,12 @@
 snap install shadowsocks-libev;
 mkdir -p /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev;
 touch /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json;
+echo "{" | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "server":"ssr.shvpnpower.cf"," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "mode":"tcp_and_udp"," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "server_port":443," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "password":"shvpnpowerssr"," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "timeout":180," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "method":"aes-256-gcm"," | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "   "nameserver":"8.8.8.8"" | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "}" | tee -a /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
