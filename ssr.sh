10fde7019e43eac1c10c66c46f58547ff77c4ba0
kruleshvpn <34224735+kruleshvpn@users.noreply.github.com>
2020-10-25 21:51:51 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,1 +1,11 @@
-s
+#!/bin/bash
+#
+# Script By Khairul SHVPN
+# SHVPN Your Internet Speed Expert
+# ==================================================
+
+apt update && apt upgrade -y;
+apt install -y snapd;
+apt install -y haveged;
+wget https://
+reboot
